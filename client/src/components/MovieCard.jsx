a9e3c059ca39b1577d1b1093cd603ec6da016edb
Nesteryonok <nastyaa.04@yandex.ru>
2026-03-19 16:18:13 +0300

    Update: new client

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -9,9 +9,7 @@ const MovieCard = ({ movie, index }) => {
         <h3 className="compact-title">{movie.name || movie.title}</h3>
         <div className="compact-meta">
           <span className="compact-genres">{movie.genres || 'Жанр не указан'}</span>
-          {movie.emotions && (
-            <span className="compact-emotion"> {movie.emotions}</span>
-          )}
+
         </div>
       </div>
     </div>
